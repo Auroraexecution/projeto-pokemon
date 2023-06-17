b9851004c9fc6eaecbd97b5c2b64882a57f05337
Auroraexecution <edgardmagic@hotmail.com>
2023-06-17 20:17:19 -0300

    Subindo o projeto inicial

---
+++ src/interapok.js
@@ -0,0 +1,16 @@
+const botaoAlterar = document.getElementById("botao-alterar")
+
+const body = document.querySelector("body")
+
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
+
+botaoAlterar.addEventListener("click", () => {
+    const modoEscuroAtivo = body.classList.contains("modo-escuro")
+    console.log("Modo escuro está ativo")
+    body.classList.toggle("modo-escuro")
+    if(modoEscuroAtivo) {
+        imagemBotaoTrocaDeTema.setAttribute("src", "/src/Imagenspok/sun.png")
+    } else {
+        imagemBotaoTrocaDeTema.setAttribute("src", "/src/Imagenspok/moon.png")
+    }
+})
